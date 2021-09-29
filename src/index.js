c09c1d74d17f817cd62a0128712dec69ad4970f0
victor castro <victor_castro_01@hotmail.com>
2021-09-29 19:05:32 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,71 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function toys(){
+  return fetch('http://localhost:3000/toys')
+  .then(res=>res.json())
+  .then(toy=> toy.forEach(t => 
+    renderToys(t),
+    ))
+}
+
+
+
+
+function renderToys(toy){
+  let card = document.createElement('li');
+  card.className = 'card'
+  card.innerHTML = `
+      
+      <h2>${toy.name}</h2>
+      <img src="${toy.image}" class="toy-avatar">
+      <p>${toy.likes}</p>
+      <button class="like-btn">Like</button>
+      
+  ` 
+  card.querySelector('button').addEventListener('click',()=> {
+        toy.likes+=1
+        card.querySelector('p').innerText = toy.likes
+        updateLikes(toy)
+  })
+  
+  document.getElementById("toy-collection").appendChild(card)
+  
+}
+
+function newToy(){
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+        Accept: "application/json"},
+    body: JSON.stringify({
+      "name": "document.querySelector('input#toy-name').value",
+      "image": "document.querySelector('input#toy-name').value",
+      "likes": 0
+    })
+  })
+  .then(res=>res.json())
+  .then(data=>console.log(data))
+}
+
+function updateLikes(toy){
+    fetch(`http://localhost:3000/toys/${toy.id}`, {
+      method: 'PATCH',
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(toy)
+    })
+    .then(res=>res.json())
+    .then(toyData => toyData)
+}
+
+document.querySelector('form').addEventListener('submit', newToy )
+document.addEventListener('DOMContentLoaded', toys)
+
+
+
+
